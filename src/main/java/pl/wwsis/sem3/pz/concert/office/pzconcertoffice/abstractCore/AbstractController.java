3539b210ae3e6f58deadb704dc9f57fb6526596b
jacek <jacek.677369@gmail.com>
2023-06-30 23:00:25 +0200

    Troche poprawione

--- src/main/java/pl/wwsis/sem3/pz/concert/office/pzconcertoffice/abstractCore/AbstractController.java
+++ src/main/java/pl/wwsis/sem3/pz/concert/office/pzconcertoffice/abstractCore/AbstractController.java
@@ -1,6 +1,9 @@
 package pl.wwsis.sem3.pz.concert.office.pzconcertoffice.abstractCore;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,22 +11,44 @@ import java.util.List;
 public abstract class AbstractController  <service extends AbstractService,DTO extends AbstractDto,ENTITY extends AbstractBaseEntity>{
     @Autowired
     private  AbstractService<DTO,ENTITY> service;
-
-
     @GetMapping("")
     public List<DTO> getAll(){
         return service.getAll();
     }
-    @GetMapping("{id}")
+    @GetMapping(":{id}")
     public DTO getById(@PathVariable("id") Long id){
         return (DTO) service.findById(id);
     }
-    @PostMapping("")
-    public void insertNew(@RequestBody DTO dto){
-        service.insertNew(dto);
+    @PostMapping("create-new")
+    public ResponseEntity<ResponseData> insertNew(@RequestBody DTO dto){
+
+        ResponseData data= new ResponseData();
+        try{
+            DTO insertedDto = service.insertNew(dto);
+
+            data.status="success";
+            data.data=insertedDto;
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(data);
+        }catch (Exception e){
+            data.status="failed";
+            data.exception=e.toString();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(data);
+        }
     }
-    @DeleteMapping("{id}")
-    public void deleteById(@PathVariable("id") Long id){
-        service.deleteById(id);
+    @DeleteMapping(":{id}")
+    public ResponseEntity<ResponseData> deleteById(@PathVariable("id") Long id){
+        ResponseData data= new ResponseData();
+        try{
+            service.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(data);
+        }catch (Exception e){
+            data.status="failed";
+            data.exception=e.toString();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(data);
+        }
     }
 }
